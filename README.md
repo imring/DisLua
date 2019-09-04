50998ec11548b902f12ddd611523025e8481b316
imring <jackringtest@gmail.com>
2019-09-04 20:22:54 +0300

    added description, update lua files.

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # DisLua
-LuaJIT 2.1 Bytecode Interpreter
+DisLua is a Lua library for parsing bytecode of compiled LuaJIT files. Supports version only LuaJIT v2.1.
+### Get list of opcodes
+Example code:
+```lua
+-- file.lua
+function add(a, b)
+    return a + b
+end
+
+local function sub(a, b)
+    return a - b
+end
+
+number_1 = 123
+local number_2 = 100
+print(add(number_1, number_2))
+print(sub(number_1, number_2))
+```
+Compile this file:
+```bash
+$ luajit.exe -b file.lua file.luac
+```
+Run file test.lua:
+```bash
+$ luajit test.lua
+Enter path to file: file.luac
+Start.
+End. Total time: 0.002 sec.
+```
+And got a list of opcodes:
+```lua
+-- BYTECODE -- file.luac:00000006-00000015
+-- size: 0000000f args: 2 opcodes: 2
+0000000d    ADDVV (32)	2	0	1
+00000011    RET1 (76)	2	2
+
+-- BYTECODE -- file.luac:00000016-00000025
+-- size: 0000000f args: 2 opcodes: 2
+0000001d    SUBVV (33)	2	0	1
+00000021    RET1 (76)	2	2
+
+-- BYTECODE -- file.luac:00000026-0000008e
+-- size: 00000068 args: 0 opcodes: 19
+0000002d    FNEW (51)	0	0 ; -- file.luac:00000006-00000015
+00000031    GSET (55)	0	1 ; "add"
+00000035    FNEW (51)	0	2 ; -- file.luac:00000016-00000025
+00000039    KSHORT (41)	1	123
+0000003d    GSET (55)	1	3 ; "number_1"
+00000041    KSHORT (41)	1	100
+00000045    GGET (54)	2	4 ; "print"
+00000049    GGET (54)	3	1 ; "add"
+0000004d    GGET (54)	4	3 ; "number_1"
+00000051    MOV (18)	5	1
+00000055    CALL (66)	3	0	3
+00000059    CALLM (65)	2	1	0
+0000005d    GGET (54)	2	4 ; "print"
+00000061    MOV (18)	3	0
+00000065    GGET (54)	4	3 ; "number_1"
+00000069    MOV (18)	5	1
+0000006d    CALL (66)	3	0	3
+00000071    CALLM (65)	2	1	0
+00000075    RET0 (75)	0	1
+```
+
+### TODO:
+* Get debuginfo.
+* Save edited protos.
--- test.lua
+++ test.lua
@@ -1,67 +1,83 @@
-local ffi = require 'ffi'
-local bit = require 'bit'
+local require = require
+local popen, write, read, open = io.popen, io.write, io.read, io.open
+local gsub, match, format = string.gsub, string.match, string.format
+local clock = os.clock
+
 local bc_read = require 'dislua.init'
-require 'tostring'
+require 'tostring' -- http://lua-users.org/wiki/TableUtils
 
-local current_path = io.popen'cd':read'*a':gsub('\n', '')
-io.write('Enter path to file: ')
-local path = io.read()
+local tabletostring = table.tostring
 
-local name = path:match('.*[\\/](.*)$') or path
-local name = name:match('(.*)%..-$') or name
+local current_path = gsub(popen('cd'):read('*a'), '\n', '')
+write('Enter path to file: ')
+local path = read()
 
-local cl = os.clock()
+local name = match(path, '.*[\\/](.*)$') or path
+name = match(name, '(.*)%..-$') or name
+local res = current_path .. '\\bc\\' .. name .. '-bc.lua'
+local file = open(res, 'w')
+
+local cl = clock()
 print('Start.')
 local info = bc_read(path)
-local res = current_path .. '\\bc\\' .. name .. '-bc.lua'
-local file = io.open(res, 'w')
 local protos = info:read_protos()
 
 local function fix_string(str)
-	local res = str:gsub('\\', '\\\\'):gsub('"', '\\"'):gsub('\n', '\\n'):gsub('\t', '\\t')
-	return res
+	str = gsub(str, '\\', '\\\\')
+	str = gsub(str, '"', '\\"')
+	str = gsub(str, '\n', '\\n')
+	str = gsub(str, '\t', '\\t')
+	return str
 end
 
 local jmps_addr = {}
 for i = 1, #protos do
 	local proto = protos[i]
-	local max = table.maxn(proto.params)
-	file:write(('-- BYTECODE -- %s:%08x-%08x\n'):format(path, proto.pos, proto.pos + proto.len))
-	file:write(('-- size: %08x args: %d opcodes: %d\n'):format(proto.len, proto.numparams, proto.sizebc))
-	for i = 1, #proto.opcodes do
-		local info = proto.opcodes[i]
-		local current_pos = info.pos
-		if info.number >= 77 and info.number <= 88 and info.number ~= 81 and info.number ~= 83 and info.number ~= 86
-		   or info.number == 72 or info.number == 50 then
-			local addr = ( info.args[2] - 0x8000 + 1 ) * 4 + current_pos
-			jmps_addr[addr] = true
-			info.args[2] = ('%08x (%d)'):format(addr, info.args[2] - 0x8000 + 1)
-		end
-	end
+	local max = #proto.params
+	file:write(format('-- BYTECODE -- %s:%08x-%08x\n', path, proto.pos, proto.pos + proto.len))
+	file:write(format('-- size: %08x args: %d opcodes: %d\n', proto.len, proto.numparams, proto.sizebc))
 	for i = 1, #proto.opcodes do
 		local info = proto.opcodes[i]
 		local current_pos = info.pos
 		local comments = ''
-		if info.number == 51 then
-			local cproto = proto.params[max - info.args[2]]
-			comments = (' ; -- %s:%08x-%08x'):format(path, cproto.pos, cproto.pos + cproto.len)
-		elseif info.number == 54 or info.number == 39 or info.number == 55 or info.number == 6 or info.number == 7 or info.number == 47 then
-			comments = (' ; "%s"'):format(fix_string(proto.params[max - info.args[2]]))
-		elseif info.number == 42 or info.number == 8 or info.number == 9 or info.number == 48 then
-			comments = (' ; %d'):format(proto.params[info.args[2] + 1])
-		elseif info.number >= 22 and info.number <= 31 then
-			comments = (' ; %d'):format(proto.params[info.args[3] + 1])
+		if info.number == 51 then -- FNEW
+			local index = max - info.args[2]
+			local cproto = proto.params[index]
+			comments = format(' ; -- %s:%08x-%08x', path, cproto.pos, cproto.pos + cproto.len)
+		elseif info.number == 54 or info.number == 39 or info.number == 55 or info.number == 6 or info.number == 7 or info.number == 47 then -- KSTR, GGET, TGETS, etc
+			local index = max - info.args[2]
+			local str = fix_string(proto.params[index])
+			comments = format(' ; "%s"', str)
+		elseif info.number == 42 or info.number == 8 or info.number == 9 or info.number == 48 then -- KNUM, USETN, etc
+			local index = info.args[2] + 1
+			comments = format(' ; %d', proto.params[index])
+		elseif info.number >= 22 and info.number <= 31 then -- http://wiki.luajit.org/Bytecode-2.0#binary-ops
+			local index = info.args[3] + 1
+			comments = format(' ; %d', proto.params[index])
 		elseif info.number == 57 or info.number == 61 then
-			comments = (' ; "%s"'):format(fix_string(proto.params[max - info.args[3]]))
+			local index = max - info.args[3]
+			local str = fix_string(proto.params[index])
+			comments = format(' ; "%s"', str)
 		elseif info.number == 53 then
-			comments = (' ; %s'):format(table.tostring(proto.params[max - info.args[2]]))
+			local index = max - info.args[2]
+			local tabstr = tabletostring(proto.params[index])
+			comments = format(' ; %s', tabstr)
+		elseif info.number >= 77 and info.number <= 88 and info.number ~= 81 and info.number ~= 83 and info.number ~= 86
+			or info.number == 72 or info.number == 50 then -- http://wiki.luajit.org/Bytecode-2.0#loops-and-branches
+			local addr = ( info.args[2] - 0x8000 + 1 ) * 4 + current_pos
+			info.args[2] = format('%08x (%d)', addr, info.args[2] - 0x8000 + 1)
 		end
-		local start = ' ' .. ( jmps_addr[current_pos] and '=>' or '  ' ) .. ' ' .. info.name
-		file:write(('%08x%s (%d)\t%s%s\n'):format(current_pos, start, info.number, table.concat(info.args, '\t'), comments))
+		local start = ' ' .. ( proto.jumps[current_pos] and '=>' or '  ' ) .. ' ' .. info.name
+		local argstr = ''
+		for i = 1, #info.args do
+			argstr = argstr .. info.args[i]
+			if i ~= #info.args then argstr = argstr .. '\t' end
+		end
+		file:write(format('%08x%s (%d)\t%s%s\n', current_pos, start, info.number, argstr, comments))
 	end
 	file:write('\n')
 end
 
-file:close()
+print(format('End. Total time: %.03f sec.', clock() - cl))
 
-print(('End. Total time: %.03f sec.'):format(os.clock() - cl))
+file:close()
